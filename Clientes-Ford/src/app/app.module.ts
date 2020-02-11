043052682b56aaa935b8531fdbac7dc0b0036576
biovieira <vieira.bio@gmail.com>
2020-02-11 00:33:46 -0300

    Inclusão do cabeçalho e esboço do CSS

--- Clientes-Ford/src/app/app.module.ts
+++ Clientes-Ford/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CabecalhoComponent } from './cabecalho/cabecalho.component';
+import { ClienteComponent } from './cliente/cliente.component';
+import { VeiculoComponent } from './veiculo/veiculo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabecalhoComponent,
+    ClienteComponent,
+    VeiculoComponent
   ],
   imports: [
     BrowserModule
